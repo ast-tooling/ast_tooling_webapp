c04de6c62e6548b0fb170b6ac01ee30a7d644604
Adam Simon <asimon@billtrust.local>
2019-07-05 11:11:03 -0400

    added models and admin function

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tool(models.Model):
+    name = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+    owner = models.CharField(max_length=255)
+
+    def __str__(self):
+        return 'AST Tool {0} owned by {1}'.format(self.name,self.owner)
