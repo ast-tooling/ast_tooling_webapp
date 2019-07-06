72b4bde63662d7b163962f2b84b442dcc416970d
Adam Simon <asimon@billtrust.local>
2019-07-05 23:01:14 -0400

    added views for tools, started templates

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('vft',views.vft,name='vft'),
+    path('prepost_compare',views.prepost,name='prepost'),
+    path('cazar',views.cazar,name='cazar'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from django.template import loader
+
+from .models import Tool
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hey this is the response')
+    l_tools = Tool.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('app/index.html')
+    context = {
+        'l_tools': l_tools,
+    }
+    return render(request,'app/index.html',context)
+
+def vft(request,tool_id):
+
+    tool = get_object_or_404(Tool,pk=tool_id)
+    return render(request,'app/vft',{'tool':tool})
+
+def prepost(request):
+    return HttpResponse('pre post time snitches')
+
+def cazar(request):
+    return HttpResponse('find this')
