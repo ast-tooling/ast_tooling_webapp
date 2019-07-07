3238a4c343a59dc4c3040ef634490bd7f2186e83
Adam Simon <asimon@billtrust.local>
2019-07-06 22:37:42 -0400

    added functionality for pages for each tool

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Tool(models.Model):
     name = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date published')
     owner = models.CharField(max_length=255)
+    url_name = models.CharField(max_length=50)
 
     def __str__(self):
         return 'AST Tool {0} owned by {1}'.format(self.name,self.owner)
--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('vft',views.vft,name='vft'),
-    path('prepost_compare',views.prepost,name='prepost'),
-    path('cazar',views.cazar,name='cazar'),
+    path('', views.index, name='index'),
+    path('vft', views.vft, name='vft'),
+    path('prepost', views.prepost, name='prepost'),
+    path('cazar', views.cazar, name='cazar'),
 ]
