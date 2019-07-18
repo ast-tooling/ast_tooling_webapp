fca068f1b7d3d0a6ead09ef84f03f959afce96c7
Adam Simon <asimon@billtrust.local>
2019-07-17 22:15:21 -0400

    added form handling to vft view

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 from .models import Tool
 from .forms import VftForm,PrePostForm
 
+import os
+
 # Create your views here.
 def index(request):
     l_tools = Tool.objects.order_by('-pub_date')[:5]
@@ -16,15 +18,30 @@ def index(request):
     return render(request,'app/index.html',context)
 
 def vft(request):
+    base = os.getcwd()
+    fname = 'vft_optin_single.csv'
     tool = Tool.objects.filter(name='vft workflow').values()[0]
     if request.method == 'POST':
         form = VftForm(request.POST)
         if form.is_valid():
+            l_vft_cust = [form.cleaned_data['prov_id'],
+                          form.cleaned_data['csr_id'],
+                          form.cleaned_data['name']
+                          ]
+            with open(base+fname,'w') as f:
+                f.write('provder id,csr id,name\n')
+                f.write(','.join(l_vft_cust))
+                f.write('\n')
+            f.close()
             context = {
                 'tool': tool,
-                'form': form
+                'form': form,
+                'prov_id': l_vft_cust[0],
+                'csr_id': l_vft_cust[1],
+                'name': l_vft_cust[2],
+                'vft_list': l_vft_cust,
             }
-            HttpResponseRedirect('/thanks/')
+            return render(request,'app/thanks.html',context)
         elif form.is_valid == False:
             HttpResponseRedirect('/no_bueno/')
     else:
@@ -48,7 +65,7 @@ def cazar(request):
     return HttpResponse('find this')
 
 def thanks(request):
-    return HttpResponse('you landed on the thanks page')
+    return render(request,'app/thanks.html')
 
 def no_bueno(request):
     return HttpResponse('this here is the no bueno page, boo')
