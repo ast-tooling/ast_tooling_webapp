e4a9aa9aa0b3d7b3599e3037095907cec982e62e
Adam Simon <asimon@billtrust.local>
2019-08-29 10:42:21 -0400

    removed appjar module, updated req.txt

--- app/prepost/compare.py
+++ app/prepost/compare.py
@@ -10,7 +10,6 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient import discovery
-from appJar import gui
 import pymongo
 from pymongo import MongoClient
 from pymongo import MongoReplicaSetClient
